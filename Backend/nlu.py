528d8aea6b21853b1bf1c499e90c0d6d346fa0f1
Harini S <harinis7102004@gmail.com>
2025-08-17 17:40:21 +0530

    Changed CORS

--- Backend/nlu.py
+++ Backend/nlu.py
@@ -95,8 +95,8 @@ vision_client = vision.ImageAnnotatorClient()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
